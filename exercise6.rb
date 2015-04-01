909ac5d739eec8c89c621a4ce5194cd15e1d9c49
Melissa D'Agruma <mdagruma@gmail.com>
2015-04-01 15:32:01 -0400

    added .include to look for a specific value

--- exercise6.rb
+++ exercise6.rb
@@ -10,3 +10,9 @@ grocery_list.push("rice")
 groceries(grocery_list)
 
 puts "There are #{grocery_list.count} items in your grocery list"
+
+if grocery_list.include? "bananas"
+	puts "You don't need to pick up bananas today"
+else
+	puts "You need to pick up bananas"
+end
